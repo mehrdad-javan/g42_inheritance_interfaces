9559909674a88f862095437e4f6da545f0fefbfd
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-04-21 09:46:17 +0200

    call impl methods

---
+++ src/main/java/se/lexicon/interfaceExample/Test.java
@@ -0,0 +1,10 @@
+package se.lexicon.interfaceExample;
+
+public class Test {
+
+  public static void main(String[] args) {
+    TelevisionImpl television= new TelevisionImpl();
+    television.menuTemplate();
+  }
+
+}
